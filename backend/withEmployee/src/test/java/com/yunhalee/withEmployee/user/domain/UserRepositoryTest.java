2e2a6504281e70ca28ec9d0be3bc1b114fa7bf90
yunhalee05 <yunhalee0506@naver.com>
2022-05-21 22:23:02 +0900

    Test: UserRepository test 구현

---
+++ backend/withEmployee/src/test/java/com/yunhalee/withEmployee/user/domain/UserRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.yunhalee.withEmployee.user.domain;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+
+    public static final int USER_PER_PAGE = 3;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User member;
+    private User ceo;
+    private User leader;
+    private User admin;
+
+    @Before
+    public void setUp() {
+        member = save(UserTest.MEMBER);
+        ceo = save(UserTest.CEO);
+        leader = save(UserTest.LEADER);
+        admin = save(UserTest.ADMIN);
+    }
+
+    @Test
+    public void find_users() {
+        Pageable pageable = PageRequest.of(0, USER_PER_PAGE);
+        Page<User> page = userRepository.findAllUsers(pageable);
+        assertThat(page.getContent().size()).isEqualTo(3);
+    }
+
+
+    private User save(User user) {
+        return userRepository.save(User.builder()
+            .name(user.getName())
+            .email(user.getEmail())
+            .password(user.getPassword())
+            .imageUrl(user.getImageUrl())
+            .description(user.getDescription())
+            .phoneNumber(user.getPhoneNumber())
+            .role(Role.valueOf(user.getRole())).build());
+    }
+
+
+}
