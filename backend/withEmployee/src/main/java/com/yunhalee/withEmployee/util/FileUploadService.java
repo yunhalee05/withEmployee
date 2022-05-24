c02a53030775f4c4fb3bfc59fd23cd52dd4332e8
yunhalee05 <yunhalee0506@naver.com>
2022-05-24 21:27:19 +0900

    Refactor: 파일 서비스 구현

--- backend/withEmployee/src/main/java/com/yunhalee/withEmployee/util/FileUploadService.java
+++ backend/withEmployee/src/main/java/com/yunhalee/withEmployee/util/FileUploadService.java
@@ -8,13 +8,43 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Transactional
 public class FileUploadService {
 
     private static String PROFILE_DIRECTORY = "profileUploads/";
+    private static String MESSAGE_DIRECTORY = "messageUploads";
+
+
+    public String uploadMessageImage(MultipartFile multipartFile) {
+        String fileName = System.currentTimeMillis() + StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        try {
+            saveFile(MESSAGE_DIRECTORY, fileName, multipartFile);
+        } catch (IOException e) {
+            new RuntimeException("Could not save file : " + multipartFile.getOriginalFilename());
+        }
+        return "/" + MESSAGE_DIRECTORY + "/" + fileName;
+    }
+
+    public void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
+        checkDirectory(uploadPath);
+        uploadFile(uploadPath, fileName, multipartFile);
+    }
+
+    private void checkDirectory(Path uploadPath) throws IOException {
+        try {
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+        } catch (IOException ex) {
+            throw new IOException("Could not create directory", ex);
+        }
+    }
 
     public String uploadProfileImage(String id, MultipartFile multipartFile) {
         String type = StringUtils.cleanPath(multipartFile.getContentType());
@@ -42,95 +72,4 @@ public class FileUploadService {
             throw new RuntimeException("Could not save file: " + fileName + ex.getMessage());
         }
     }
-
-//    public void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) {
-//        Path uploadPath = Paths.get(uploadDir);
-//
-//        if(!Files.exists(uploadPath)) {
-//            Files.createDirectories(uploadPath);
-//        }
-//
-//        try(InputStream inputStream = multipartFile.getInputStream()) {
-//            Path filePath = uploadPath.resolve(fileName);
-//            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-//        }catch (IOException ex){
-//            throw new RuntimeException("Could not save file: " +fileName + ex.getMessage());
-//        }
-//    }
-
-//    private void checkProfileDirectory(Path uploadPath) throws IOException {
-//        checkDirectory(uploadPath);
-//
-//
-//    }
-//
-//    private void emptyDirectory(Path uploadPath) throws IOException {
-//        try {
-//            Files.list(uploadPath).forEach(file -> deleteProfileFile(file));
-//        } catch (IOException e) {
-//            throw new RuntimeException("Could not list directory : " + uploadPath);
-//        }
-//    }
-//
-//    private void deleteDirectoryFiles(Path filePath) throws IOException {
-//        if (Files.isDirectory(filePath)) {
-//            emptyDirectory(filePath);
-//
-//        }
-//    }
-//
-//
-//
-//
-//
-//    public void cleanDir(Path uploadPath) {
-//        try {
-//            Files.list(uploadPath).forEach(file->{
-//                if(!Files.isDirectory(file)) {
-//                    try {
-//                        Files.delete(file);
-//                    }catch(IOException ex) {
-//                        System.out.println("Could not delete file : " + file);
-//                    }
-//                }
-//            });
-//        }catch(IOException ex2) {
-//            System.out.println("Could not list directory : " + dirPath);
-//        }
-//    }
-//
-//
-//    private void checkDirectory(Path uploadPath) throws IOException {
-//        if(!Files.exists(uploadPath)) {
-//            Files.createDirectories(uploadPath);
-//        }
-//    }
-//
-////    private void deleteFile(Path filePath) {
-////        if(!Files.isDirectory(filePath)) {
-////            try {
-////                Files.delete(filePath);
-////            }catch(IOException ex) {
-////                System.out.println("Could not delete file : " + filePath);
-////            }
-////        }
-////    }
-//    private void deleteProfileFile(Path filePath) {
-//        if (!Files.isDirectory(filePath)) {
-//            deleteFile(filePath);
-//        }
-//    }
-
-//    private void deleteFile(Path filePath) throws IOException {
-//        if (Files.isDirectory(filePath)) {
-//            throw new IOException("This is directory not file.");
-//        }
-//        try {
-//            Files.deleteIfExists(filePath);
-//        } catch (IOException e) {
-//            throw new RuntimeException("Could not delete file : " + filePath);
-//        }
-//    }
-
-
 }
