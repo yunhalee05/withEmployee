47440041f55e9215b0cc230b02e5b43a3ebd340e
yunhalee05 <yunhalee0506@naver.com>
2022-06-04 18:52:38 +0900

    Refactor: api prefix 수정

--- backend/withEmployee/src/main/java/com/yunhalee/withEmployee/config/WebSocketConfig.java
+++ backend/withEmployee/src/main/java/com/yunhalee/withEmployee/config/WebSocketConfig.java
@@ -12,7 +12,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/chat")
+            .setAllowedOrigins("http://localhost:3000")
+            .withSockJS();
     }
 
     @Override
--- backend/withEmployee/src/main/java/com/yunhalee/withEmployee/user/controller/UserController.java
+++ backend/withEmployee/src/main/java/com/yunhalee/withEmployee/user/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
